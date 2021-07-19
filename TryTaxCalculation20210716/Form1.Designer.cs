a07d789db349bbb93159b6e89c9c35998c4e0196
YI-RUE TESNG <gary135606531yrag@gmail.com>
2021-07-19 14:20:25 +0800

    ver1.1

---
+++ TryTaxCalculation20210716/Form1.Designer.cs
@@ -0,0 +1,242 @@
+﻿
+namespace TryTaxCalculation20210716
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.radioButton1_allYear = new System.Windows.Forms.RadioButton();
+            this.radioButton2_duration = new System.Windows.Forms.RadioButton();
+            this.label5_from = new System.Windows.Forms.Label();
+            this.label6_to = new System.Windows.Forms.Label();
+            this.comboBox1_use = new System.Windows.Forms.ComboBox();
+            this.comboBox2_cc = new System.Windows.Forms.ComboBox();
+            this.dateTimePicker1_startDate = new System.Windows.Forms.DateTimePicker();
+            this.dateTimePicker2_endDate = new System.Windows.Forms.DateTimePicker();
+            this.button1_calculate = new System.Windows.Forms.Button();
+            this.button2_reset = new System.Windows.Forms.Button();
+            this.label5_result = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "使用期間";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 84);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 15);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "用途";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 139);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(147, 15);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "汽缸CC數／馬達馬力";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 195);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(67, 15);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "試算結果";
+            // 
+            // radioButton1_allYear
+            // 
+            this.radioButton1_allYear.AutoSize = true;
+            this.radioButton1_allYear.Location = new System.Drawing.Point(120, 22);
+            this.radioButton1_allYear.Name = "radioButton1_allYear";
+            this.radioButton1_allYear.Size = new System.Drawing.Size(73, 19);
+            this.radioButton1_allYear.TabIndex = 4;
+            this.radioButton1_allYear.TabStop = true;
+            this.radioButton1_allYear.Text = "全年度";
+            this.radioButton1_allYear.UseVisualStyleBackColor = true;
+            this.radioButton1_allYear.CheckedChanged += new System.EventHandler(this.radioButton1_allYear_CheckedChanged);
+            // 
+            // radioButton2_duration
+            // 
+            this.radioButton2_duration.AutoSize = true;
+            this.radioButton2_duration.Location = new System.Drawing.Point(255, 22);
+            this.radioButton2_duration.Name = "radioButton2_duration";
+            this.radioButton2_duration.Size = new System.Drawing.Size(73, 19);
+            this.radioButton2_duration.TabIndex = 5;
+            this.radioButton2_duration.TabStop = true;
+            this.radioButton2_duration.Text = "依期間";
+            this.radioButton2_duration.UseVisualStyleBackColor = true;
+            this.radioButton2_duration.CheckedChanged += new System.EventHandler(this.radioButton2_duration_CheckedChanged);
+            // 
+            // label5_from
+            // 
+            this.label5_from.AutoSize = true;
+            this.label5_from.Location = new System.Drawing.Point(388, 24);
+            this.label5_from.Name = "label5_from";
+            this.label5_from.Size = new System.Drawing.Size(22, 15);
+            this.label5_from.TabIndex = 6;
+            this.label5_from.Text = "從";
+            // 
+            // label6_to
+            // 
+            this.label6_to.AutoSize = true;
+            this.label6_to.Location = new System.Drawing.Point(652, 24);
+            this.label6_to.Name = "label6_to";
+            this.label6_to.Size = new System.Drawing.Size(22, 15);
+            this.label6_to.TabIndex = 7;
+            this.label6_to.Text = "至";
+            // 
+            // comboBox1_use
+            // 
+            this.comboBox1_use.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1_use.FormattingEnabled = true;
+            this.comboBox1_use.Items.AddRange(new object[] {
+            "機車",
+            "貨車",
+            "大客車",
+            "自用小客車",
+            "營業用小客車"});
+            this.comboBox1_use.Location = new System.Drawing.Point(195, 81);
+            this.comboBox1_use.Name = "comboBox1_use";
+            this.comboBox1_use.Size = new System.Drawing.Size(209, 23);
+            this.comboBox1_use.TabIndex = 10;
+            this.comboBox1_use.SelectedIndexChanged += new System.EventHandler(this.comboBox1_use_SelectedIndexChanged);
+            // 
+            // comboBox2_cc
+            // 
+            this.comboBox2_cc.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox2_cc.FormattingEnabled = true;
+            this.comboBox2_cc.Location = new System.Drawing.Point(195, 136);
+            this.comboBox2_cc.Name = "comboBox2_cc";
+            this.comboBox2_cc.Size = new System.Drawing.Size(209, 23);
+            this.comboBox2_cc.TabIndex = 11;
+            // 
+            // dateTimePicker1_startDate
+            // 
+            this.dateTimePicker1_startDate.Location = new System.Drawing.Point(427, 22);
+            this.dateTimePicker1_startDate.Name = "dateTimePicker1_startDate";
+            this.dateTimePicker1_startDate.Size = new System.Drawing.Size(200, 22);
+            this.dateTimePicker1_startDate.TabIndex = 15;
+            // 
+            // dateTimePicker2_endDate
+            // 
+            this.dateTimePicker2_endDate.Location = new System.Drawing.Point(695, 22);
+            this.dateTimePicker2_endDate.Name = "dateTimePicker2_endDate";
+            this.dateTimePicker2_endDate.Size = new System.Drawing.Size(200, 22);
+            this.dateTimePicker2_endDate.TabIndex = 16;
+            // 
+            // button1_calculate
+            // 
+            this.button1_calculate.Location = new System.Drawing.Point(498, 301);
+            this.button1_calculate.Name = "button1_calculate";
+            this.button1_calculate.Size = new System.Drawing.Size(75, 23);
+            this.button1_calculate.TabIndex = 17;
+            this.button1_calculate.Text = "button1";
+            this.button1_calculate.UseVisualStyleBackColor = true;
+            this.button1_calculate.Click += new System.EventHandler(this.button1_calculate_Click);
+            // 
+            // button2_reset
+            // 
+            this.button2_reset.Location = new System.Drawing.Point(708, 301);
+            this.button2_reset.Name = "button2_reset";
+            this.button2_reset.Size = new System.Drawing.Size(75, 23);
+            this.button2_reset.TabIndex = 18;
+            this.button2_reset.Text = "button2";
+            this.button2_reset.UseVisualStyleBackColor = true;
+            this.button2_reset.Click += new System.EventHandler(this.button2_reset_Click);
+            // 
+            // label5_result
+            // 
+            this.label5_result.AutoSize = true;
+            this.label5_result.Location = new System.Drawing.Point(128, 195);
+            this.label5_result.Name = "label5_result";
+            this.label5_result.Size = new System.Drawing.Size(43, 15);
+            this.label5_result.TabIndex = 19;
+            this.label5_result.Text = "label5";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(979, 453);
+            this.Controls.Add(this.label5_result);
+            this.Controls.Add(this.button2_reset);
+            this.Controls.Add(this.button1_calculate);
+            this.Controls.Add(this.dateTimePicker2_endDate);
+            this.Controls.Add(this.dateTimePicker1_startDate);
+            this.Controls.Add(this.comboBox2_cc);
+            this.Controls.Add(this.comboBox1_use);
+            this.Controls.Add(this.label6_to);
+            this.Controls.Add(this.label5_from);
+            this.Controls.Add(this.radioButton2_duration);
+            this.Controls.Add(this.radioButton1_allYear);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.RadioButton radioButton1_allYear;
+        private System.Windows.Forms.RadioButton radioButton2_duration;
+        private System.Windows.Forms.Label label5_from;
+        private System.Windows.Forms.Label label6_to;
+        private System.Windows.Forms.ComboBox comboBox1_use;
+        private System.Windows.Forms.ComboBox comboBox2_cc;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1_startDate;
+        private System.Windows.Forms.DateTimePicker dateTimePicker2_endDate;
+        private System.Windows.Forms.Button button1_calculate;
+        private System.Windows.Forms.Button button2_reset;
+        private System.Windows.Forms.Label label5_result;
+    }
+}
+
---
+++ TryTaxCalculation20210716/Form1.cs
@@ -0,0 +1,291 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+//using System.Globalization;
+
+namespace TryTaxCalculation20210716
+{
+    public partial class Form1 : Form
+    {
+
+        
+        #region Events
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.label5_from.Visible = false; //隱藏日期
+            this.label6_to.Visible = false;
+            this.dateTimePicker1_startDate.Visible = false;
+            this.dateTimePicker2_endDate.Visible = false;
+            this.label5_result.Text = "";
+
+            dateTimePicker1_startDate.Format = DateTimePickerFormat.Custom;
+            dateTimePicker2_endDate.Format = DateTimePickerFormat.Custom;
+            dateTimePicker1_startDate.CustomFormat = $"民國{dateTimePicker1_startDate.Value.Year-1911}年MM月dd日";
+            dateTimePicker2_endDate.CustomFormat = $"民國{dateTimePicker2_endDate.Value.Year - 1911}年MM月dd日";
+        }
+
+        private void radioButton1_allYear_CheckedChanged(object sender, EventArgs e)
+        {
+            this.label5_from.Visible = false; //隱藏日期
+            this.label6_to.Visible = false;
+            this.dateTimePicker1_startDate.Visible = false;
+            this.dateTimePicker2_endDate.Visible = false;
+            this.comboBox1_use.SelectedItem = null;
+            this.comboBox2_cc.SelectedItem = null;
+            this.label5_result.Text = "";
+        }
+
+        private void radioButton2_duration_CheckedChanged(object sender, EventArgs e)
+        {
+            this.label5_from.Visible = true; //顯示日期
+            this.label6_to.Visible = true;
+            this.dateTimePicker1_startDate.Visible = true;
+            this.dateTimePicker2_endDate.Visible = true;
+            this.comboBox1_use.SelectedItem = null;
+            this.comboBox2_cc.SelectedItem = null;
+            this.label5_result.Text = "";
+        }
+
+
+        private void comboBox1_use_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            this.comboBox2_cc.Items.Clear();
+            if (comboBox1_use.SelectedItem as string == "機車")
+            {
+                this.comboBox2_cc.Items.Add("150以下 / 12HP以下(12.2PS以下)");
+                this.comboBox2_cc.Items.Add("151-250 / 12.1-20HP(12.3-20.3PS)");
+                this.comboBox2_cc.Items.Add("251-500 / 20.1HP以上(20.4PS以上)");
+                this.comboBox2_cc.Items.Add("501-600");
+                this.comboBox2_cc.Items.Add("601-1200");
+                this.comboBox2_cc.Items.Add("1201-1800");
+                this.comboBox2_cc.Items.Add("1801或以上");
+            }
+            else if (comboBox1_use.SelectedItem as string == "貨車")
+            {
+                this.comboBox2_cc.Items.Add("500以下");
+                this.comboBox2_cc.Items.Add("501-600");
+                this.comboBox2_cc.Items.Add("601-1200");
+                this.comboBox2_cc.Items.Add("1201-1800");
+                this.comboBox2_cc.Items.Add("1801-2400");
+                this.comboBox2_cc.Items.Add("2401-3000 / 138HP以下(140.1PS以下)");
+                this.comboBox2_cc.Items.Add("3001-3600");
+                this.comboBox2_cc.Items.Add("3601-4200 / 138.1-200HP(140.2-203.0PS)");
+                this.comboBox2_cc.Items.Add("4201-4800");
+                this.comboBox2_cc.Items.Add("4801-5400 / 200.1-247HP(203.1-250.7PS)");
+                this.comboBox2_cc.Items.Add("5401-6000");
+                this.comboBox2_cc.Items.Add("6001-6600 / 247.1-286HP(250.8-290.3PS)");
+                this.comboBox2_cc.Items.Add("6601-7200");
+                this.comboBox2_cc.Items.Add("7201-7800 / 286.1-336HP(290.4-341.0PS)");
+                this.comboBox2_cc.Items.Add("7801-8400");
+                this.comboBox2_cc.Items.Add("8401-9000 / 336.1-361HP(341.1-366.4PS)");
+                this.comboBox2_cc.Items.Add("9001-9600");
+                this.comboBox2_cc.Items.Add("9601-10200 / 361.1HP以上(366.5PS以上)");
+                this.comboBox2_cc.Items.Add("10201以上");
+            }
+            else if (comboBox1_use.SelectedItem as string == "大客車")
+            {
+                this.comboBox2_cc.Items.Add("600以下");
+                this.comboBox2_cc.Items.Add("601-1200");
+                this.comboBox2_cc.Items.Add("1201-1800");
+                this.comboBox2_cc.Items.Add("1801-2400");
+                this.comboBox2_cc.Items.Add("2401-3000 / 138HP以下(140.1PS以下)");
+                this.comboBox2_cc.Items.Add("3001-3600");
+                this.comboBox2_cc.Items.Add("3601-4200 / 138.1-200HP(140.2-203.0PS)");
+                this.comboBox2_cc.Items.Add("4201-4800");
+                this.comboBox2_cc.Items.Add("4801-5400 / 200.1-247HP(203.1-250.7PS)");
+                this.comboBox2_cc.Items.Add("5401-6000");
+                this.comboBox2_cc.Items.Add("6001-6600 / 247.1-286HP(250.8-290.3PS)");
+                this.comboBox2_cc.Items.Add("6601-7200");
+                this.comboBox2_cc.Items.Add("7201-7800 / 286.1-336HP(290.4-341.0PS)");
+                this.comboBox2_cc.Items.Add("7801-8400");
+                this.comboBox2_cc.Items.Add("8401-9000 / 336.1-361HP(341.1-366.4PS)");
+                this.comboBox2_cc.Items.Add("9001-9600");
+                this.comboBox2_cc.Items.Add("9601-10200 / 361.1HP以上(366.5PS以上)");
+                this.comboBox2_cc.Items.Add("10201以上");
+            }
+            else if (comboBox1_use.SelectedItem as string == "自用小客車")
+            {
+                this.comboBox2_cc.Items.Add("500以下 / 38HP以下(38.6PS以下)");
+                this.comboBox2_cc.Items.Add("501~600 / 38.1-56HP(38.7-56.8PS)");
+                this.comboBox2_cc.Items.Add("601~1200 / 56.1-83HP(56.9-84.2PS)");
+                this.comboBox2_cc.Items.Add("1201~1800 / 83.1-182HP(84.3-184.7PS)");
+                this.comboBox2_cc.Items.Add("1801~2400 / 182.1-262HP(184.8-265.9PS)");
+                this.comboBox2_cc.Items.Add("2401~3000 / 262.1-322HP(266-326.8PS)");
+                this.comboBox2_cc.Items.Add("3001-4200 / 322.1-414HP(326.9-420.2PS)");
+                this.comboBox2_cc.Items.Add("4201-5400 / 414.1-469HP(420.3-476.0PS)");
+                this.comboBox2_cc.Items.Add("5401-6600 / 469.1-509HP(476.1-516.6PS)");
+                this.comboBox2_cc.Items.Add("6601-7800 / 509.1HP以上(516.7PS以上)");
+                this.comboBox2_cc.Items.Add("7801以上");
+            }
+            else if(comboBox1_use.SelectedItem as string == "營業用小客車")
+            {
+                this.comboBox2_cc.Items.Add("500以下 / 38HP以下(38.6PS以下)");
+                this.comboBox2_cc.Items.Add("501~600 / 38.1-56HP(38.7-56.8PS)");
+                this.comboBox2_cc.Items.Add("601~1200 / 56.1-83HP(56.9-84.2PS)");
+                this.comboBox2_cc.Items.Add("1201~1800 / 83.1-182HP(84.3-184.7PS)");
+                this.comboBox2_cc.Items.Add("1801~2400 / 182.1-262HP(184.8-265.9PS)");
+                this.comboBox2_cc.Items.Add("2401~3000 / 262.1-322HP(266-326.8PS)");
+                this.comboBox2_cc.Items.Add("3001-4200 / 322.1-414HP(326.9-420.2PS)");
+                this.comboBox2_cc.Items.Add("4201-5400 / 414.1-469HP(420.3-476.0PS)");
+                this.comboBox2_cc.Items.Add("5401-6600 / 469.1-509HP(476.1-516.6PS)");
+                this.comboBox2_cc.Items.Add("6601-7800 / 509.1HP以上(516.7PS以上)");
+                this.comboBox2_cc.Items.Add("7801以上");
+            }
+        }
+        
+        
+        private void button1_calculate_Click(object sender, EventArgs e) //計算結果
+        {
+            if (radioButton1_allYear.Checked)
+            {
+                this.GetAllYear();
+            }
+            else
+            {
+                this.GetDuration();
+            }
+        }
+
+        private void button2_reset_Click(object sender, EventArgs e) //重製
+        {
+            this.FormReset();
+        }
+        #endregion
+
+        #region Custom Methods
+
+        int leapOrNot;
+        /// <summary>計算年值</summary>
+        private void GetAllYear()
+        {
+            int laepYear = DateTime.Now.Year;
+            if (DateTime.IsLeapYear(laepYear)) //判斷是否為閏年
+            {
+                leapOrNot = 366;
+            }
+            else
+            {
+                leapOrNot = 365;
+            }
+
+            if (comboBox1_use.SelectedItem == null || comboBox2_cc.SelectedItem == null)
+                {
+                    this.label5_result.Text = "請選擇正確用途及CC數";
+                    return;
+                }
+            else
+            {
+                this.TaxCalculate();
+            }
+
+            this.label5_result.Text = $@"
+             使用期間:民國{dateTimePicker1_startDate.Value.Year - 1911}年01月01日 ~ 民國{dateTimePicker2_endDate.Value.Year - 1911}年12月31日
+             計算天數:365天
+             汽缸CC數:{comboBox1_use.SelectedItem as string}
+             用途:{comboBox2_cc.SelectedItem as string}
+             計算公式:{taxValue}*{leapOrNot}/{leapOrNot} = {taxValue * leapOrNot / leapOrNot}元
+             應納稅額: 共{taxValue}元";
+        }
+       
+        int dateDuration;
+        /// <summary>計算日期差值</summary>
+        private void GetDuration()
+        {
+            //CultureInfo cui = new CultureInfo("zh-TW", true); 切換格式
+            //cui.DateTimeFormat.Calendar = new TaiwanCalendar();
+            //dateTimePicker1_startDate.CustomFormat = dateTimePicker1_startDate.Value.ToString("yy/M/d", cui);
+           
+
+            DateTime dateStart = dateTimePicker1_startDate.Value.Date;
+            DateTime dateEnd = dateTimePicker2_endDate.Value.Date;
+            dateDuration = new TimeSpan(dateEnd.Ticks - dateStart.Ticks).Days;
+
+            if (comboBox1_use.SelectedItem == null || comboBox2_cc.SelectedItem == null)
+            {
+                this.label5_result.Text = "請選擇正確用途及CC數";
+                return;
+            }
+            else if (dateStart >= dateEnd)
+            {
+                this.label5_result.Text = "請選擇正確日期";
+                return;
+            }
+            else
+            {
+                this.TaxCalculate();
+            }
+            
+            this.label5_result.Text = $@"
+             使用期間:民國{ dateTimePicker1_startDate.Value.Year - 1911}年{dateTimePicker1_startDate.Value.Month}月{dateTimePicker1_startDate.Value.Day}日 ~ 民國{dateTimePicker2_endDate.Value.Year - 1911}年{dateTimePicker2_endDate.Value.Month}月{dateTimePicker2_endDate.Value.Day}日
+             計算天數:{Convert.ToString(dateDuration)}天
+             汽缸CC數:{comboBox1_use.SelectedItem as string}
+             用途:{comboBox2_cc.SelectedItem as string}
+             計算公式:{taxValue}*{dateDuration}/365 = {taxValue * dateDuration / 365}元
+             應納稅額: 共{taxValue * dateDuration / 365}元";
+        }
+
+        /// <summary>控制項初始化</summary>
+        private void FormReset()
+        {
+            this.label5_from.Visible = false; //隱藏日期
+            this.label6_to.Visible = false;
+            this.dateTimePicker1_startDate.Value = DateTime.Today;
+            this.dateTimePicker2_endDate.Value = DateTime.Today;
+            this.dateTimePicker1_startDate.Visible = false;
+            this.dateTimePicker2_endDate.Visible = false;
+            this.radioButton1_allYear.Checked = true;
+            this.label5_result.Text = "";
+            this.comboBox1_use.SelectedItem = null;
+            this.comboBox2_cc.SelectedItem = null;
+        }
+
+        int taxValue;
+        /// <summary>稅務公式 </summary>
+        private void TaxCalculate()
+        {
+            if (comboBox1_use.SelectedItem as string == "機車" && comboBox2_cc.SelectedItem as string == "150以下 / 12HP以下(12.2PS以下)")
+            {
+                taxValue = 0;
+            }
+            else if (comboBox1_use.SelectedItem as string == "機車" && comboBox2_cc.SelectedItem as string == "151-250 / 12.1-20HP(12.3-20.3PS)")
+            {
+                taxValue = 800;
+            }
+            else if (comboBox1_use.SelectedItem as string == "機車" && comboBox2_cc.SelectedItem as string == "251-500 / 20.1HP以上(20.4PS以上)")
+            {
+                taxValue = 1620;
+            }
+            else if (comboBox1_use.SelectedItem as string == "機車" && comboBox2_cc.SelectedItem as string == "501-600")
+            {
+                taxValue = 2160;
+            }
+            else if (comboBox1_use.SelectedItem as string == "機車" && comboBox2_cc.SelectedItem as string == "601-1200")
+            {
+                taxValue = 4320;
+            }
+            else if (comboBox1_use.SelectedItem as string == "機車" && comboBox2_cc.SelectedItem as string == "1201-1800")
+            {
+                taxValue = 7120;
+            }
+            else if (comboBox1_use.SelectedItem as string == "機車" && comboBox2_cc.SelectedItem as string == "1801或以上")
+            {
+                taxValue = 11230;
+            }
+        }
+        #endregion
+
+
+    }
+}
